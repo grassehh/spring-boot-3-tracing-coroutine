9391b6e473f0a3d2bf4446ca09269ed9f0a3d572
Sylvain Becuwe <sylvain_becuwe@connect-tech.sncf>
2023-09-14 23:54:48 +0200

    chore: Bump dependencies and added platforms

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,10 +1,11 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import org.springframework.boot.gradle.plugin.SpringBootPlugin.BOM_COORDINATES
 
 plugins {
-    id("org.springframework.boot") version "3.1.1"
+    id("org.springframework.boot") version "3.1.3"
     id("io.spring.dependency-management") version "1.1.0"
-    kotlin("jvm") version "1.8.22"
-    kotlin("plugin.spring") version "1.8.22"
+    kotlin("jvm") version "1.9.10"
+    kotlin("plugin.spring") version "1.9.10"
 }
 
 group = "com.grassehh"
@@ -19,6 +20,9 @@ repositories {
 }
 
 dependencies {
+    implementation(platform(BOM_COORDINATES))
+    implementation(platform("org.zalando:logbook-bom:3.1.0"))
+    implementation(platform("io.micrometer:micrometer-tracing-bom:1.1.5"))
     implementation("org.springframework.boot:spring-boot-starter-webflux")
     implementation("org.springframework.boot:spring-boot-starter-security")
     runtimeOnly("org.springframework.boot:spring-boot-starter-actuator")
@@ -27,12 +31,10 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-slf4j:1.7.2")
-    implementation("io.projectreactor.netty:reactor-netty-http-brave:1.1.8")
-    implementation("io.micrometer:micrometer-tracing:1.1.2")
-    implementation("io.micrometer:micrometer-tracing-bridge-brave:1.1.2")
+    implementation("io.micrometer:micrometer-tracing-bridge-brave")
     implementation("io.github.microutils:kotlin-logging:3.0.5")
-    implementation("org.zalando:logbook-spring-boot-starter:3.1.0")
-    implementation("org.zalando:logbook-spring-boot-webflux-autoconfigure:3.1.0")
+    implementation("org.zalando:logbook-spring-boot-starter")
+    implementation("org.zalando:logbook-spring-boot-webflux-autoconfigure")
     compileOnly("org.springframework.boot:spring-boot-devtools")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("io.projectreactor:reactor-test")
